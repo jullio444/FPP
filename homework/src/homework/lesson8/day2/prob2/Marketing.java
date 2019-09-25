9a9fbfebd4bdd721640c3a29d51e7fea5bdc8821
jullio444 <54864892+jullio444@users.noreply.github.com>
2019-09-24 20:14:25 -0500

    Update Marketing.java

--- homework/src/homework/lesson8/day2/prob2/Marketing.java
+++ homework/src/homework/lesson8/day2/prob2/Marketing.java
@@ -1,5 +1,7 @@
 package homework.lesson8.day2.prob2;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Objects;
 
 public class Marketing {
@@ -44,5 +46,37 @@ public class Marketing {
 
 	public String getProductName() {
 		return productname;
+	}
+	 public static ArrayList<Marketing> greaterThan1000(ArrayList<Marketing> am){
+		ArrayList<Marketing> amDupe = new ArrayList<Marketing>();
+		for(Marketing m: am) {
+			if(m.getSalesAmount()>1000) {
+				amDupe.add(m);
+			}
+		}
+		return amDupe;
+	}
+	public static void main(String[] args) {
+		ArrayList<Marketing> m = new ArrayList<Marketing>();
+		m.add(new Marketing("Jullio Jymez", "Apple", 7500));
+		m.add(new Marketing("Kevin Heart", "Apple", 800));
+		m.add(new Marketing("King Saha", "Dell", 1600));
+		m.add(new Marketing("Mike Luda", "Dell", 1200));
+		
+		Collections.sort(m, (e1, e2) -> {
+			return ((Double)e1.getSalesAmount()).compareTo(e2.getSalesAmount());
+		});
+		System.out.println("Sorted by Sales amount");
+		m.forEach(System.out::println);
+		System.out.println();
+		
+		ArrayList<Marketing> greater = new ArrayList<Marketing>();
+		greater = greaterThan1000(m);
+		Collections.sort(greater, (e1,e2)->{
+			return e1.getEmployeeName().compareTo(e2.getEmployeeName());
+		});
+		
+		System.out.println("Sorted by Name with the criteria of the employee who achieves more than $1000");
+		greater.forEach(System.out::println);
 	}
 }
